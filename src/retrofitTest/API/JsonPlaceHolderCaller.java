da5118388fa06b1d4630bba70fce6fcededa9c00
ludwings0330 <ludwings0330@gmail.com>
2022-10-05 16:11:56 +0900

    gradle 변경

--- src/retrofitTest/API/JsonPlaceHolderCaller.java
+++ src/retrofitTest/API/JsonPlaceHolderCaller.java
@@ -1,2 +1,27 @@
-package retrofitTest.API;public class JsonPlaceHolderCaller {
+package retrofitTest.API;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class JsonPlaceHolderCaller {
+    private final static String baseURL = "https://jsonplaceholder.typicode.com/";
+    static JsonPlaceHolderAPI instance;
+
+    public static JsonPlaceHolderAPI getInstance() {
+        if (instance == null) {
+            instance = createInstance().create(JsonPlaceHolderAPI.class);
+        }
+        return instance;
+    }
+
+    private static Retrofit createInstance() {
+        final Gson gson = new GsonBuilder().setLenient().create();
+
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(baseURL)
+                .build();
+    }
 }
--- src/retrofitTest/RetrofitTest.java
+++ src/retrofitTest/RetrofitTest.java
@@ -1,2 +1,96 @@
-package retrofitTest;public class RetrofitTest {
+package retrofitTest;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofitTest.API.JsonPlaceHolderAPI;
+import retrofitTest.API.JsonPlaceHolderCaller;
+import retrofitTest.DTO.PostDto;
+
+import java.io.IOException;
+import java.util.List;
+
+public class RetrofitTest {
+    static JsonPlaceHolderAPI api;
+
+    public static void main(String[] args) throws IOException {
+        api = JsonPlaceHolderCaller.getInstance();
+        getOneByIdTest(1l);
+        getAllTest();
+        savePostTest();
+        modifyTest(2l);
+        patchTest(1L);
+        getOneByUserIdTest(1L);
+    }
+
+    private static void getOneByUserIdTest(long userId) throws IOException {
+        final Call<List<PostDto.Post>> call = api.getOneByUserId(userId);
+        final Response<List<PostDto.Post>> response = call.execute();
+        final List<PostDto.Post> posts = response.body();
+
+
+        System.out.println("---------------- get one by user id --------------------");
+
+        for (var post :
+                posts) {
+            System.out.println(post);
+        }
+    }
+
+    private static void patchTest(long id) throws IOException {
+        final PostDto.Post post = new PostDto.Post();
+        post.setTitle("foo...");
+        final Call<PostDto.Post> call = api.patchPost(post, id);
+        final Response<PostDto.Post> response = call.execute();
+        final PostDto.Post result = response.body();
+        System.out.println("------------- patch Test -------------");
+        System.out.println(result);
+    }
+
+    private static void modifyTest(long id) throws IOException {
+        final PostDto.Post post = new PostDto.Post();
+        post.setId(id);
+        post.setUserId(1L);
+        post.setTitle("modify Title");
+        post.setBody("modify Body");
+        final Call<PostDto.Post> call = api.modifyPost(post, id);
+        final Response<PostDto.Post> response = call.execute();
+        final PostDto.Post responsePost = response.body();
+
+        System.out.println("------------- modify Test -------------");
+        System.out.println(responsePost);
+    }
+
+    private static void savePostTest() throws IOException {
+        final PostDto.Request request = new PostDto.Request();
+        request.setTitle("test Title");
+        request.setBody("test body");
+        request.setUserId(1l);
+
+        final Call<PostDto.Post> call = api.savePost(request);
+        final Response<PostDto.Post> response = call.execute();
+        final PostDto.Post post = response.body();
+
+        System.out.println("---------- save Post Test ----------");
+        System.out.println(post);
+    }
+
+    private static void getAllTest() throws IOException {
+        System.out.println("--------- getAll ----------");
+        final Call<List<PostDto.Post>> all = api.getAll();
+        final Response<List<PostDto.Post>> response = all.execute();
+        final List<PostDto.Post> posts = response.body();
+        for (var post :
+                posts) {
+            System.out.println(post);
+        }
+    }
+
+    private static void getOneByIdTest(Long id) throws IOException {
+        final Call<PostDto.Post> call = api.getOneByPostId(id);
+        final Response<PostDto.Post> postResponse = call.execute();
+        final PostDto.Post post = postResponse.body();
+
+        System.out.println("--------- getOneByPostID ---------");
+        System.out.println(post);
+    }
 }
