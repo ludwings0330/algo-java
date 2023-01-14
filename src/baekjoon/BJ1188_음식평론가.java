5f1094ee7a215753c38c7e72db032fbffec3065f
ludwings <81959681+ludwings0330@users.noreply.github.com>
2023-01-15 01:21:50 +0900

    Algo(Baekjoon): PS
    - BJ1188_음식평론가

--- src/baekjoon/BJ1188_음식평론가.java
+++ src/baekjoon/BJ1188_음식평론가.java
@@ -1,2 +1,29 @@
-package baekjoon;public class BJ1188_음식평론가 {
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1188_음식평론가 {
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(M - gcd(N, M));
+    }
+
+    public static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
 }
