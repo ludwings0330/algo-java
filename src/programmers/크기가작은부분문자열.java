2adc07ae454c5cded4f03bc80fa4027d843606d1
ludwings0330 <ludwings0330@gmail.com>
2023-01-17 09:14:23 +0900

    Algo(Programmers): PS
    - 테이블 해시 함수 (lv2)
    - 정렬

--- src/programmers/크기가작은부분문자열.java
+++ src/programmers/크기가작은부분문자열.java
@@ -1,2 +1,24 @@
-package programmers;public class 크기가작은부분문자열 {
+package programmers;
+
+public class 크기가작은부분문자열 {
+
+    public static void main(String[] args) {
+//        System.out.println(solution("3141592", "271"));
+        System.out.println(solution("10203", "15"));
+    }
+
+    public static int solution(String t, String p) {
+        int answer = 0;
+
+        int len = p.length();
+        Long pp = Long.valueOf(p);
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            if (Long.valueOf(t.substring(i, i + len)) <= pp) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
 }
